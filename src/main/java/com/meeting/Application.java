679601f1993d0cfefb6248e93bcbb58eb285464e
liumingyue0203 <liumingyue0203@163.com>
2016-09-25 18:37:02 +0800

    liumy spring boot 实例

--- src/main/java/com/meeting/Application.java
+++ src/main/java/com/meeting/Application.java
@@ -52,7 +52,7 @@ public class Application  {
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class);
         logger.info("SpringBoot Start Success");
     }
 
